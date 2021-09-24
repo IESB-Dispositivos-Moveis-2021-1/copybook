c3bf0d449ddf89627d624e4a5ec2616d1dcae813
Pedro Henrique <intellitour@gmail.com>
2021-09-23 22:52:54 -0300

    Exemplo Aula 02

---
+++ Copybook/ViewController/FeedTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FeedTableViewController.swift
+//  Copybook
+//
+//  Created by Pedro Henrique on 23/09/21.
+//
+
+import UIKit
+
+class FeedTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        FeedTableViewCell.register(in: tableView)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.kReuseIdentifier, for: indexPath) as! FeedTableViewCell
+        
+        cell.setup(with: nil)
+    
+        return cell
+    }
+    
+    
+
+}
